916fe9e3e8ca41afa770ce8dbf3a0a2114eceb65
Pranav910 <pranavsuryavanshi910@gmail.com>
2023-08-25 12:43:50 +0530

    update commit

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  server : {
-    proxy : {
-      '/new_registration' : 'http://localhost:5000',
-      '/user-login' : 'http://localhost:5000',
-      '/streaks-option' : 'http://localhost:5000',
-    }
-  },
+  // server : {
+  //   proxy : {
+  //     '/new_registration' : 'http://localhost:5000',
+  //     '/user-login' : 'http://localhost:5000',
+  //     '/streaks-option' : 'http://localhost:5000',
+  //     '/logout' : 'http://localhost:5000',
+  //   }
+  // },
   plugins: [react()],
   
 })
